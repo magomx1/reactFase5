eedab1228d19342f6f052caa24764a57e71f9781
magomx1 <mgomezfl@unadvirtual.edu.co>
2023-05-28 22:08:37 -0500

    Update React1
    Despliegue React

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Diseño de Sitios Web</h1>
+      <div className="card">
+        <h6>Enlace Proyecto</h6>
+        <a href="index.html">Click aquí para ir al proyecto</a>
+      </div>
+      <p className="read-the-docs">
+        Mauricio Gómez Flórez
+      </p>
+    </>
+  )
+}
+
+export default App
